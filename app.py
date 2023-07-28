dac9db8a2f5da5de88327389d200a93d80ad3dae
dachrisch <christian.daehn@gmail.com>
2023-07-28 10:55:10 +0200

    skipping tests

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import uvicorn as uvicorn
 
+import os
+
 if __name__ == '__main__':
-    uvicorn.run("api.routes:app", host='0.0.0.0', port=5000, reload=True)
+    reload = os.getenv('OS_FRONTEND_RELOAD', False)
+    app = os.getenv('OS_FRONTEND_APP', "api.routes:app")
+    port = os.getenv('OS_FRONTEND_PORT', 5000)
+    uvicorn.run(app, host='0.0.0.0', port=port, reload=reload)
